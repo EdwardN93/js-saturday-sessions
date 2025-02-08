6706b8e275356893c457e0ade5be0d6839e3c5a7
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-02-08 10:19:49 +0200

    added the first challenge

---
+++ session-01/ex-01/main.js
@@ -0,0 +1,20 @@
+console.log("main.js loaded");
+/* 
+Programul preia de la utilizator o valoare numerica si afiseaza
+   1. intr-un singur mesaj, o secventa de text ce contine: numarului anterior, numarul primit, numarul succesiv (ex. 5 => "4, 5, 6")
+   2. opusul numarului (ex. 5 => "opusul numarului 5 este -5", -3 => "opusul numarului -3 este 3")
+   3. restul impartirii la 2 (ex. 5 => "restul impartirii lui 5 la 2 este 1")
+   4. 2 la puterea valorii (ex. 3 => "2 la puterea 3 este 8")
+*/
+
+// 1)
+let inputNr = prompt("Input a number");
+inputNr = Number(inputNr) - 1;
+
+let sequence = [];
+
+for (let x = 0; x < 3; x++) {
+  sequence.push(inputNr++);
+}
+
+alert(sequence.join(", "));
