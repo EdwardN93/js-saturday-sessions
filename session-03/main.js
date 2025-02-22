8d1028ba94b7e13cbd133d611cdad6430b45ae62
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-02-22 12:23:57 +0200

    session 3 added

--- session-03/main.js
+++ session-03/main.js
@@ -1,7 +1,38 @@
 // 15 min challenge 1
 
-function diceRoll(dice) {
-  return console.log(`You rolled: ${Math.floor(Math.random() * dice) + 1}`);
+function diceRoll(dice = 6) {
+  return Math.floor(Math.random() * dice) + 1;
 }
 
-diceRoll(12);
+// 15 min challenge 2
+
+function rollMultipleDice(dices = 2) {
+  const rollsPerDice = [];
+  if (dices > 1) {
+    for (let i = 0; i < dices; i++) {
+      rollsPerDice.push(diceRoll());
+    }
+    return rollsPerDice;
+  } else {
+    rollsPerDice.push(diceRoll());
+  }
+  return rollsPerDice;
+}
+
+// 15 min challenge 3
+
+let rewardsForWinning = [
+  "$50 USD american dollars",
+  "a 3 days vacation in Hell",
+  "emmotional damage",
+  "an arrow in the knee",
+  "a Js course",
+  "nothing",
+  "a Dog",
+];
+
+function rewardDice(reward) {
+  return reward[Math.floor(Math.random() * reward.length)];
+}
+
+console.log(`Congratulations ! You earned: ${rewardDice(rewardsForWinning)}`);
