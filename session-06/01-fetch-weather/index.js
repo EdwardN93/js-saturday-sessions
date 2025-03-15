243be50a0d512fd1bfcbfb5fa3854c56b7061bd9
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-03-15 14:04:15 +0200

    forecast day added

---
+++ session-06/01-fetch-weather/index.js
@@ -0,0 +1,63 @@
+console.log("Fetch explained!");
+
+/**
+ * Fetch current forecast
+ */
+// fetch(
+//   `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=Bucharest`
+// )
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((apiData) => {
+//     console.log(apiData);
+//   });
+
+/**
+ * Fetch current forecast by hour
+ */
+
+fetch(
+  `https://api.weatherapi.com/v1/forecast.json?days=1&key=${weatherApiKey}&q=Bucuresti`
+)
+  .then((response) => response.json())
+  .then((apiData) => displayWeather(apiData));
+
+function displayWeather(apiData) {
+  const forecastDay = apiData.forecast.forecastday[0];
+  const image = apiData.current.condition.icon;
+  const curHour = new Date().getHours();
+  const weatherAtThisHour = forecastDay.hour[curHour];
+
+  console.log(forecastDay);
+
+  document.querySelector(".img").src = `https:${image}`;
+  document.querySelector(
+    ".celsius"
+  ).textContent = `${weatherAtThisHour.temp_c}°C`;
+  document.querySelector(".condition").textContent =
+    weatherAtThisHour.condition.text;
+  document.querySelector(".date").textContent = forecastDay.date
+    .split("-")
+    .reverse()
+    .join("-");
+
+  console.log(weatherAtThisHour.time.split(" ")[1]);
+
+  forecastDay.hour.forEach((element) => {
+    const html = `
+    <div class="forecast">
+    <p>${element.time.split(" ")[1]}</p>
+    <img class="widget-img" src="https:${image}" alt="weather.img" />
+    <p>${element.temp_c}°C</p>
+    </div>
+    `;
+
+    document
+      .querySelector(".forecast-hours")
+      .insertAdjacentHTML("beforeend", html);
+    console.log(element);
+  });
+
+  console.log(weatherAtThisHour);
+}
